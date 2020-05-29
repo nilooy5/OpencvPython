e28c2c0127f66c7becb6af02b5ea8708359ecdca
Fazal Mahmud Niloy <niloooy@gmail.com>
2020-05-30 02:57:57 +0600

    videoCaptured from webcam.

--- chapter1.py
+++ chapter1.py
@@ -1,7 +1,11 @@
 import cv2
-print("package imported")
 
-img = cv2.imread("Resources/lena.png")
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
 
-cv2.imshow("output", img)
-cv2.waitKey(0)
+while True:
+    success, img = cap.read()
+    cv2.imshow("Video", img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
